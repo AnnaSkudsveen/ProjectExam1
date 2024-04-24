2987aef11196b6775f0df62d010e0eb04e6a30f0
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-04-24 21:50:07 +0200

    chore: collected data from form to js

---
+++ account/js/register.js
@@ -0,0 +1,5 @@
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const registerBtn = document.querySelector(".registerBtn");
+const logInBtn = document.querySelector(".logInBtn");
