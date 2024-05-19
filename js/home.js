f559d65b000ca90d408544e9d7a437b1c2f1613b
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-05-19 15:16:29 +0200

    Showing all posts on homepage

--- js/home.js
+++ js/home.js
@@ -1,4 +1,6 @@
-const
+const headerSection = document.querySelector(".header-section");
+const carouselSection = document.querySelector(".carousel-section");
+const blogPostSection = document.querySelector(".blog-post-overview");
 
 function getPosts() {
   try {
@@ -6,15 +8,62 @@ function getPosts() {
       .then((response) => response.json())
       .then((data) => {
         showPosts(data);
+        showHeader(data);
+        carousel(data);
       });
   } catch (error) {
     console.log("an error has happened");
   }
 }
 
-function showPosts(postData) {
-  console.log(postData);
+function showHeader(postData) {
+  console.log(postData.data[0]);
+  headerSection.innerHTML += `
+    <section class="headerPost">
+    <img src="${postData.data[0].media}" alt="image linked to post">
+    <h2>${postData.data[0].title}</h2>
+    <p>${postData.data[0].body}</p>
+    <button>Read more</button>
+    </section>
+      `;
+}
 
+function carousel(postData) {
+  carouselSection.innerHTML += `
+    <section class="carouselPost">
+    <img src="${postData.data[0].media}" alt="image linked to post">
+    <h2>${postData.data[0].title}</h2>
+    <p>${postData.data[0].body}</p>
+    <button>Read more</button>
+    </section>
+
+    <section class="carouselPost">
+    <img src="${postData.data[1].media}" alt="image linked to post">
+    <h2>${postData.data[1].title}</h2>
+    <p>${postData.data[1].body}</p>
+    <button>Read more</button>
+    </section>
+
+    <section class="carouselPost">
+    <img src="${postData.data[2].media}" alt="image linked to post">
+    <h2>${postData.data[2].title}</h2>
+    <p>${postData.data[2].body}</p>
+    <button>Read more</button>
+    </section>
+      `;
+}
+
+function showPosts(postData) {
+  for (let i = 0; i < postData.data.length; i++) {
+    blogPostSection.innerHTML += `
+    <section class="blog-post">
+    <img src="${postData.data[i].media}" alt="">
+    <h2>${postData.data[i].title}</h2>
+    <p>${postData.data[i].body}</p>
+    <button>Read more</button>
+    </section>
+      `;
+  }
 }
 
 getPosts();
