b63f1f13207bb8b99337ba1261dc02d6a4e15e7d
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-05-20 13:56:00 +0200

    used API params to send post id and show it on post/index

---
+++ js/blogPost.js
@@ -0,0 +1,31 @@
+const blogPost = document.querySelector(".post");
+
+const parameterString = window.location.search;
+const searchParameters = new URLSearchParams(parameterString);
+const postId = searchParameters.get("id");
+console.log(postId);
+
+function getPosts() {
+  try {
+    fetch(`https://v2.api.noroff.dev/blog/posts/AnnaSkudsveen/${postId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        showPosts(data);
+      });
+  } catch (error) {
+    console.log("an error has happened");
+  }
+}
+
+function showPosts(postData) {
+  blogPost.innerHTML += `
+      <img src="${postData.data.media}" alt="">
+      <h1>${postData.data.title}</h1>
+      <p>${postData.data.body}</p>
+        `;
+}
+
+getPosts();
+
+// const title = post.title;
+// document.title = `${title} || HotView Labs`;
--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 const headerSection = document.querySelector(".header-section");
 const carouselSection = document.querySelector(".carousel-section");
 const blogPostSection = document.querySelector(".blog-post-overview");
-const readMoreBtn = document.querySelector(".read-more-btn");
+const readMoreBtn = document.querySelectorAll(".read-more-btn");
 
 function getPosts() {
   try {
@@ -20,53 +20,53 @@ function getPosts() {
 function showHeader(postData) {
   console.log(postData.data[0]);
   headerSection.innerHTML += `
+  <a class="post-link-card" href="post/index.html?id=${postData.data[0].id}">
     <section class="headerPost">
     <img src="${postData.data[0].media}" alt="image linked to post">
     <h2>${postData.data[0].title}</h2>
     <p>${postData.data[0].body}</p>
     <button class="read-more-btn">Read more</button>
     </section>
+    </a>
       `;
 }
 
 function carousel(postData) {
-  carouselSection.innerHTML += `
+  for (let i = 0; i < 3; i++) {
+    carouselSection.innerHTML += `
+    <a class="post-link-card" href="post/index.html?id=${postData.data[i].id}">
     <section class="carouselPost">
-    <img src="${postData.data[0].media}" alt="image linked to post">
-    <h2>${postData.data[0].title}</h2>
-    <p>${postData.data[0].body}</p>
+    <img src="${postData.data[i].media}" alt="image linked to post">
+    <h2>${postData.data[i].title}</h2>
+    <p>${postData.data[i].body}</p>
     <button>Read more</button>
     </section>
+    </a>
+    `;
 
-    <section class="carouselPost">
-    <img src="${postData.data[1].media}" alt="image linked to post">
-    <h2>${postData.data[1].title}</h2>
-    <p>${postData.data[1].body}</p>
-    <button>Read more</button>
-    </section>
-
-    <section class="carouselPost">
-    <img src="${postData.data[2].media}" alt="image linked to post">
-    <h2>${postData.data[2].title}</h2>
-    <p>${postData.data[2].body}</p>
-    <button>Read more</button>
-    </section>
-      `;
+    // readMoreBtn.addEventListener("click", () => {
+    //   const postId = postData.data[i].id;
+    //   console.log(postId);
+    //   window.location.replace(`/post/index.html/?id=${postId}`);
+    // });
+  }
 }
+
+//Fix: so theres pagination that shows 12 and 12
 
 function showPosts(postData) {
   for (let i = 0; i < postData.data.length; i++) {
     blogPostSection.innerHTML += `
+    <a class="post-link-card" href="post/index.html?id=${postData.data[i].id}">
     <section class="blog-post">
     <img src="${postData.data[i].media}" alt="">
     <h2>${postData.data[i].title}</h2>
     <p>${postData.data[i].body}</p>
     <button>Read more</button>
     </section>
+    </a>
       `;
   }
 }
 
 getPosts();
-
-readMoreBtn.addEventListener("click", () => {});
