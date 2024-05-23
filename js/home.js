53ddb92d75318de3c965296548b7a0b27a5e5263
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-05-23 17:03:01 +0200

    Tried pagination, failed

--- js/home.js
+++ js/home.js
@@ -3,14 +3,53 @@ const carouselSection = document.querySelector(".carousel-section");
 const blogPostSection = document.querySelector(".blog-post-overview");
 const readMoreBtn = document.querySelectorAll(".read-more-btn");
 
+function shortenText(text) {
+  const words = text.split(" ");
+  const wordsShort = words.slice(0, 20).join(" ") + "...";
+  return wordsShort;
+}
+
+function paginate(items, itemsPerPage) {
+  const totalPages = Math.ceil(items.length / itemsPerPage);
+  const pages = [];
+
+  for (let i = 0; i < totalPages; i++) {
+    const start = i * itemsPerPage;
+    const end = start + itemsPerPage;
+    pages.push(items.slice(start, end));
+  }
+
+  return pages;
+  // showPosts(pages);
+}
+
 function getPosts() {
   try {
     fetch("https://v2.api.noroff.dev/blog/posts/AnnaSkudsveen")
       .then((response) => response.json())
       .then((data) => {
-        showPosts(data);
         showHeader(data);
+        console.log(showHeader(data));
         carousel(data);
+        console.log(carousel(data));
+        const paginatedPosts = paginate(data, 12);
+        for (const post in paginatedPosts[0]) {
+          showPosts(post);
+        }
+
+        let counter = 1;
+        for (const postArray of paginatedPosts) {
+          const pageButton = document.createElement("button");
+          pageButton.innerText = counter;
+          counter++;
+          pageButton.addEventListener("click", () => {
+            blogPostSection.innerHTML = "  ";
+            for (const book of postArray) {
+              displayBook(book);
+            }
+          });
+          pageButtons.appendChild(pageButton);
+        }
       });
   } catch (error) {
     console.log("an error has happened");
@@ -18,15 +57,13 @@ function getPosts() {
 }
 
 function showHeader(postData) {
-  console.log(postData.data[0]);
   headerSection.innerHTML += `
   <a class="post-link-card" href="post/index.html?id=${postData.data[0].id}">
     <section class="headerPost">
-    <img src="${postData.data[0].media.url}" alt="${
-    postData.data[i].media.alt
-  }">
+    <img src="${postData.data[0].media.url}" 
+    alt="${postData.data[0].media.alt}">
     <h2>${postData.data[0].title}</h2>
-    <p>${shortenText(postData.data[i].body)}</p>
+    <p>${shortenText(postData.data[0].body)}</p>
     <button class="read-more-btn">Read more</button>
     </section>
     </a>
@@ -38,9 +75,8 @@ function carousel(postData) {
     carouselSection.innerHTML += `
     <a class="post-link-card" href="post/index.html?id=${postData.data[i].id}">
     <section class="carouselPost">
-    <img src="${postData.data[i].media.url}" alt="${
-      postData.data[i].media.alt
-    }">
+    <img src="${postData.data[i].media.url}" 
+    alt="${postData.data[i].media.alt}">
     <h2>${postData.data[i].title}</h2>
     <p>${shortenText(postData.data[i].body)}</p>
     <button>Read more</button>
@@ -56,27 +92,36 @@ function carousel(postData) {
   }
 }
 
-function shortenText(text) {
-  const words = text.split(" ");
-  const wordsShort = words.slice(0, 20).join(" ") + "...";
-  return wordsShort;
-}
-
-//Fix: so theres pagination that shows 12 and 12
-
 function showPosts(postData) {
-  for (let i = 0; i < postData.data.length; i++) {
+  console.log(postData);
+  for (let i = 0; i < postData.length; i++) {
     blogPostSection.innerHTML += `
-    <a class="post-link-card" href="post/index.html?id=${postData.data[i].id}">
+    <a class="post-link-card" href="post/index.html?id=${postData[i].id}">
     <section class="blog-post">
-    <img src="${postData.data[i].media.url}" alt="">
-    <h2>${postData.data[i].title}</h2>
-    <p>${shortenText(postData.data[i].body)}</p>
+    <img src="${postData[i].media.url}" alt="">
+    <h2>${postData[i].title}</h2>
+    <p>${shortenText(postData[i].body)}</p>
     <button>Read more</button>
     </section>
     </a>
       `;
   }
 }
+// function showPosts(postData) {
+//   for (let i = 0; i < postData.data.length; i++) {
+//     blogPostSection.innerHTML += `
+//     <a class="post-link-card" href="post/index.html?id=${postData.data[i].id}">
+//     <section class="blog-post">
+//     <img src="${postData.data[i].media.url}" alt="">
+//     <h2>${postData.data[i].title}</h2>
+//     <p>${shortenText(postData.data[i].body)}</p>
+//     <button>Read more</button>
+//     </section>
+//     </a>
+//       `;
+//   }
+// }
+
+//Fix: so theres pagination that shows 12 and 12
 
 getPosts();
